e9b9b62feb791e3cb71ba1f718a7bf40bdd617dc
kat markava <katryc@outlook.com>
2016-02-22 10:59:57 -0500

    added params and create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new #setting up a template user for the form
   end
+  def created
+    @user = User.new(user_params)
+     return render action: 'new' unless @user.save
+      redirect_to root path, notice: 'created a new user'
+end
+
+private
+def user_params
+  prams.
+    require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
