13ca585d3810f0b822f64f92a24ce53d038026d0
kat markava <katryc@outlook.com>
2016-02-22 11:14:14 -0500

    added session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new #setting up a template user for the form
   end
-  def created
+  def create
     @user = User.new(user_params)
      return render action: 'new' unless @user.save
       redirect_to root path, notice: 'created a new user'
@@ -10,7 +10,7 @@ end
 
 private
 def user_params
-  prams.
+  params.
     require(:user).permit(:username, :password, :password_confirmation)
   end
 end
